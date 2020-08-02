985844715e717cc4b3823f1356c03ed16f197aeb
HanJin Park <phj9020@hotmail.com>
2020-08-02 21:22:39 +0900

    CanvasClick EventListner(fill) added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const canvas = document.getElementById("js-canvas");
 const ctx = canvas.getContext("2d");
 const color = document.getElementsByClassName("jsColor");
+const range = document.getElementById("js-range");
+const mode = document.getElementById("jsMode");
+
+// 기본 컬러  stroke & fill
+const INITIAL_COLOR = "#2c2c2c";
 
 // pixel manipulating size : pixel modifier에 사이즈를 줌 /
 canvas.width = 1200;
 canvas.height = 700;
 
 //default 선의 기본값 색상 & 라인 두께
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 //마우스가 움직이는 내내 발생
 function onMouseMove(event) {
@@ -36,9 +43,33 @@ function startPainting() {
   painting = true;
 }
 
-function handleColor(event){
+function handleColor(event) {
   const color = event.target.style.backgroundColor;
-  ctx.strokeStyle = color;  // overwrite default stroke style 
+  ctx.strokeStyle = color; // overwrite default stroke style
+  ctx.fillStyle = ctx.strokeStyle; // fill 값에도 같은 color를 적용
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+// Fill & Paint Mode button text change
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+// canvas click for Fill
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, 1200, 700); // pixel manipulating size
+  }; 
 }
 
 if (canvas) {
@@ -46,10 +77,20 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
+// Color Change Event Listner
 Array.from(color).forEach((color) =>
   color.addEventListener("click", handleColor)
 );
 
+//Brush Size (range) :: event Listener
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
 
+// Button jsMode :: event Listenr
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
